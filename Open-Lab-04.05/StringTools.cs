382f9fc79b54e8d51d2bd23693b7ba42f980e401
martinorion <71751917+martinorion@users.noreply.github.com>
2021-01-10 14:56:42 +0100

    Repeat Letters N Times

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -4,9 +4,17 @@ namespace Open_Lab_04._05
 {
     public class StringTools
     {
-        public string Repeat(string orig, int n)
+         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string r = "";
+            for (int i = 0; i < orig.Length; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    r = r + orig[i];
+                }
+            }
+            return r;
         }
     }
 }
